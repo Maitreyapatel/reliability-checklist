702d581c14d5cf7732b8b80385b3bb59a6251c30
Maitreyapatel <patel.maitreya57@gmail.com>
2022-12-21 15:29:32 -0700

    limit test set to 1per

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,7 @@ def cfg_eval_global() -> DictConfig:
         with open_dict(cfg):
             cfg.paths.root_dir = str(pyrootutils.find_root())
             cfg.trainer.max_epochs = 1
-            cfg.trainer.limit_test_batches = 0.1
+            cfg.trainer.limit_test_batches = 0.01
             cfg.trainer.accelerator = "cpu"
             cfg.datamodule.num_workers = 0
             cfg.datamodule.pin_memory = False
