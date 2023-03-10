ecf9c9743d32b1d591876102342ae9f62e36b35f
Maitreya Patel <patel.maitreya57@gmail.com>
2023-03-09 23:19:19 -0700

    style changes

--- reliability_score/datamodules/common_datamodule.py
+++ reliability_score/datamodules/common_datamodule.py
@@ -1,6 +1,4 @@
 import logging
-
-import logging
 from typing import Any, Dict, Optional, Tuple
 
 import torch
@@ -9,9 +7,9 @@ from pytorch_lightning import LightningDataModule
 from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
 
 from reliability_score.datamodules.utils import (
-    process_label2id,
-    general_tokenization,
     conversion_process,
+    general_tokenization,
+    process_label2id,
 )
 
 
@@ -79,9 +77,7 @@ class GeneralDataModule(LightningDataModule):
         if self.data_processing.columns:
             for column_name, column_prefix in self.data_processing.columns.items():
                 dataset = dataset.map(
-                    lambda example: {
-                        column_name: " ".join([column_prefix, example[column_name]])
-                    },
+                    lambda example: {column_name: " ".join([column_prefix, example[column_name]])},
                     batched=False,
                 )
 
@@ -144,9 +140,7 @@ class GeneralDataModule(LightningDataModule):
         logging.info("Performing tokenization...")
         old_columns = set(list(self.data_test.features.keys()))
         self.data_test = self.data_test.map(self.tokenization.process, batched=True)
-        self.label_conversion = process_label2id(
-            self.label2id, self.tokenizer_data.label2id
-        )
+        self.label_conversion = process_label2id(self.label2id, self.tokenizer_data.label2id)
         self.data_test = self.data_test.map(
             lambda batch: {"converted_label": self.label_conversion[batch["label"]]},
             batched=False,
@@ -180,9 +174,7 @@ class GeneralDataModule(LightningDataModule):
 
     def setup(self, stage: Optional[str] = None):
         if not self.data_test:
-            logging.error(
-                "It seems that dataset object was not declared. Attempting it again."
-            )
+            logging.error("It seems that dataset object was not declared. Attempting it again.")
             self.prepare_data()
 
     def train_dataloader(self):
